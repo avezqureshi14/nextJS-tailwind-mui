da5c4eeb454f509ccf3d1ea376c2a8a99d4d2a1d
avezqureshi <aveznqureshi14@gmail.com>
2024-03-13 23:26:50 +0530

    feat: header added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Button from "./components/Button"
+import Header from './components/Header'
+function App() {
+  return (
+    <>
+
+      <Header />
+      <ButtonGradient />
+    </>
+  )
+}
+
+export default App
